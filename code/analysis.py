a263173fb9f35dc84980b1e82b74fb981b0b4304
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-07-11 19:01:10 -0400

    Formatting.

--- code/analysis.py
+++ code/analysis.py
@@ -48,7 +48,7 @@ for topic in topics_model0_l0.columns:
     top_ten_model_0_l0.append(topics_model0_l0[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l0.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l0.xlsx')
 for i, A in enumerate(top_ten_model_0_l0):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -60,7 +60,7 @@ for topic in topics_model0_l1.columns:
     top_ten_model_0_l1.append(topics_model0_l1[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l1.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l1.xlsx')
 for i, A in enumerate(top_ten_model_0_l1):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -71,7 +71,7 @@ for topic in topics_model0_l2.columns:
     top_ten_model_0_l2.append(topics_model0_l2[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l2.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model0_l2.xlsx')
 for i, A in enumerate(top_ten_model_0_l2):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -104,7 +104,7 @@ for topic in topics_model1_l0.columns:
     top_ten_model_1_l0.append(topics_model1_l0[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l0.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l0.xlsx')
 for i, A in enumerate(top_ten_model_1_l0):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -116,7 +116,7 @@ for topic in topics_model1_l1.columns:
     top_ten_model_1_l1.append(topics_model1_l1[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l1.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l1.xlsx')
 for i, A in enumerate(top_ten_model_1_l1):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -128,7 +128,7 @@ for topic in topics_model1_l2.columns:
     top_ten_model_1_l2.append(topics_model1_l2[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l2.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model1_l2.xlsx')
 for i, A in enumerate(top_ten_model_1_l2):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -162,7 +162,7 @@ for topic in topics_model2_l0.columns:
     top_ten_model_2_l0.append(topics_model2_l0[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l0.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l0.xlsx')
 for i, A in enumerate(top_ten_model_2_l0):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -174,7 +174,7 @@ for topic in topics_model2_l1.columns:
     top_ten_model_2_l1.append(topics_model2_l1[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l1.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l1.xlsx')
 for i, A in enumerate(top_ten_model_2_l1):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
@@ -186,31 +186,11 @@ for topic in topics_model2_l2.columns:
     top_ten_model_2_l2.append(topics_model2_l2[topic].nlargest(10))
 
 # save
-writer=pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l2.xlsx')
+writer = pd.ExcelWriter('/home/brandon/Documents/topsbm_preprocessing/output/model2_l2.xlsx')
 for i, A in enumerate(top_ten_model_2_l2):
     A.to_excel(writer,sheet_name=f"topic_{i}")
 writer.save()
 
-# # how many docs belong to each topic
-# pd.DataFrame(model.groups_[0]['p_tw_d'])
-# pd.DataFrame(model.groups_[1]['p_tw_d'])
-# model2_l2_topic_counts = pd.DataFrame(model.groups_[2]['p_tw_d']).transpose()
-# model2_l2_topic_counts['ident'] = model2_l2_topic_counts.index
-# model2_l2_topic_counts = model2_l2_topic_counts.melt(id_vars=['ident'], var_name=['topic'])
-
-# grouped = model2_l2_topic_counts.groupby(by = ['ident'])
-# grouped.apply(lambda g: g[g.value == g.value.max()])
-
-# model2_l2_topic_counts.groupby('ident').filter(lambda x : x['value'].max() == x.value)
-# model2_l2_topic_counts.loc[model2_l2_topic_counts.groupby(["ident"])["value"].idxmax()]
-
-# idx = model2_l2_topic_counts.groupby(['ident'])['value'].transform(max) == model2_l2_topic_counts['value']
-# model2_l2_topic_counts[idx]
-# # number of docs per topic
-# model2_l2_topic_counts[idx].topic.value_counts().sort_index()
-
-# # look at counts per documnt
-# model2_l2_topic_counts[idx].groupby('ident')['topic'].nunique().max()
 
 
 get_topic_counts(model, 0)
